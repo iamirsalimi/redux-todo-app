e0a6cad462803a005a905b5fa747e4ca55a9140b
iamirsalimi <wonderguy2003@gmail.com>
2025-07-25 19:10:21 +0330

    filter todos added to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,15 @@ import { useState } from 'react'
 
 import "./App.css";
 import TodoList from "./Components/TodoList/TodoList";
-import { useDispatch } from 'react-redux'
-import { addTodo } from './Redux/Store/Todos'
-import { GoPlus } from "react-icons/go";
 
 export default function App() {
-  const [title , setTitle] = useState('')
-
-  const dispatch = useDispatch()
-
-
-  const addNewTodo = e => {
-    e.preventDefault()
-    dispatch(addTodo({id : crypto.randomUUID() , title , isDone : false}))
-    setTitle('')
-  }
 
   return (
     <>
       <header>
         <h1>TodoList</h1>
       </header>
-      <form action="">
-        <input type="text" className="todo-input !text-gray-700 text-xl" value={title} onChange={e => setTitle(e.target.value)} />
-        <button className="todo-button flex items-center justify-center max-w-fit" type="submit" onClick={addNewTodo}>
-          <GoPlus className="" />
-        </button>
-        <div className="select">
-          <select name="todos" className="filter-todo bg-white">
-            <option value="all">All</option>
-            <option value="completed">Completed</option>
-            <option value="incomplete">Incomplete</option>
-          </select>
-        </div>
-      </form>
-
-      <div className="todo-container">
-        <ul className="todo-list">
-          <TodoList />
-        </ul>
-      </div>
-
+      <TodoList />
     </>
   )
 }
--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,20 +1,55 @@
+import { useState } from 'react'
+import { useSelector , useDispatch } from "react-redux";
+
+import { addTodo } from '../../Redux/Store/Todos'
+import { GoPlus } from "react-icons/go";
+
 import Todo from "../Todo/Todo";
-import { useSelector } from "react-redux";
+
 export default function TodoList() {
+    const [title, setTitle] = useState('')
+    const [filterTodo, setFilterTodo] = useState('all')
     const todos = useSelector(state => state)
+
+    const dispatch = useDispatch()
+
+    const addNewTodo = e => {
+        e.preventDefault()
+        dispatch(addTodo({ id: crypto.randomUUID(), title, isDone: false }))
+        setTitle('')
+    }
 
     return (
         <>
-            {todos.map((todo) => (
-                <Todo key={todo.id} {...todo} />
-            ))}
+            <form action="">
+                <input type="text" className="todo-input !text-gray-700 text-xl" value={title} onChange={e => setTitle(e.target.value)} />
+                <button className="todo-button flex items-center justify-center max-w-fit" type="submit" onClick={addNewTodo}>
+                    <GoPlus className="" />
+                </button>
+                <div className="select">
+                    <select name="todos" className="filter-todo bg-white" value={filterTodo} onChange={e => setFilterTodo(e.target.value)}>
+                        <option value="all">All</option>
+                        <option value="completed">Completed</option>
+                        <option value="incomplete">Incomplete</option>
+                    </select>
+                </div>
+            </form>
+
+            <div className="todo-container">
+                <ul className="todo-list">
+                    {filterTodo == 'all' && todos.map((todo) => (
+                        <Todo key={todo.id} {...todo} />
+                    ))}
+
+                    {filterTodo == 'completed' && todos.filter(todo => todo.isDone).map((todo) => (
+                        <Todo key={todo.id} {...todo} />
+                    ))}
+
+                    {filterTodo == 'incomplete' && todos.filter(todo => !todo.isDone).map((todo) => (
+                        <Todo key={todo.id} {...todo} />
+                    ))}
+                </ul>
+            </div>
         </>
     );
 }
-
-// function mapStateToProps(state) {
-//     console.log(state)
-//     return { todos: state };
-// }
-
-// export default connect(mapStateToProps)(TodoList);
